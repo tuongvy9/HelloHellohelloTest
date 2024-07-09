bd241d6e585441ebcacf2d7cf2ced11e02b37581
Vy you <98216975+tuongvy9@users.noreply.github.com>
2024-07-10 03:23:42 +0700

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            creator: 't_furu',
+            url: 'https://vrmeditor.tools',
+            image: 'https://vrmeditor.tools/res/ogimage.png',
+            title: 'VRMEditorTools',
+            description: 'ブラウザでVRMのテクスチャを変更できるツール',
+            hashtags: 'VRMEditorTools'
+        }
+    }
+}
